6621c1f1c660d27d493d3893e3441b49fc36b40e
tmoroney14 <thomas.moroney1010@gmail.com>
2019-08-25 17:57:24 +0100

    4 files modified

--- src/app/buskers.ts
+++ src/app/buskers.ts
@@ -6,6 +6,8 @@ export const buskers = [
     videos: 'https://www.youtube.com/embed/videoseries?list=PLkNQ3IPg_TnUYqGHwWVdEq50pNR16T9iH',
     live: 'https://www.youtube.com/embed/videoseries?list=TLGGQ_HuxeraarkyMjA4MjAxOQ&index=1',
     music1: 'https://open.spotify.com/embed/artist/2yTmZ1PwFKkO0FjQp2flPl',
+    music2: '',
+
     name: 'Allie Sherlock',
     description: 'Aged 14, Allie comes down from Cork with her dad every weekend to busk on Grafton Street.'
   },
@@ -17,14 +19,24 @@ export const buskers = [
     live: 'https://www.youtube.com/embed/videoseries?list=UUQcfc9A8QgHGo4hjWx7DFjA',
     music1: 'https://www.youtube.com/embed/wUEtpfJUs7Q',
     music2: '',
+
     name: 'Padraig Cahill',
-    description: 'Padraig is aged 16 and from Wicklow. He regularly takes the bus to Dublin to busk on Grafton Street, drawing large crowds.'
+    description: 'Padraig is aged 16 and from Wicklow. He regularly takes the bus to Dublin to busk on Grafton Street, drawing large crowds.',
+    description2: 'Padraig is aged 16 and from Wicklow. He regularly takes the bus to Dublin to busk on Grafton Street, drawing large crowds.'
   },
   {
     header: 'https://scontent.fdub5-1.fna.fbcdn.net/v/t1.0-9/18010165_1039411549492152_5036536940223665555_n.jpg?_nc_cat=101&_nc_oc=AQm9jIj0uE-7W37pNG370V3aH1NwNjhWWsjxK9G6j4kv1ysqqbskihgDnHxVKxWGPAM&_nc_ht=scontent.fdub5-1.fna&oh=f7096f4a39a6ed4611034e5fd8fac2cd&oe=5DB2DA8C',
     image: 'https://i.ytimg.com/vi/Zn-Y3nlBzSk/maxresdefault.jpg',
+    other: 'Performance',
+    videos: 'https://www.youtube.com/embed/videoseries?list=PL7Hzq1i4Oc9CzAs4sbMegFQZDQuxtIras',
+    live: 'https://www.youtube.com/embed/A8oG7m5Tr2M',
+    music1: 'https://open.spotify.com/embed/artist/54NoJVgezmrMKZp2G1wznx',
+    music2: '',
+
     name: 'Jacob Coopman',
-    description: 'Jacob is a Singer/Songwriter that can be found playing venues around Ireland and busking on Grafton Street playing a mixture of original and cover music.'
+    description: 'Jacob is a Singer/Songwriter that can be found playing venues around Ireland and busking on Grafton Street playing a mixture of original and cover music.',
+    para1: 'Jacob’s Bali roots provides him with an indie pop style but his early influences such as Dave Grohl and Chris Cornell give an edge to his island vibe. He is a Singer/Songwriter who started playing drums at 9 years old. Self taught he also plays the guitar and bass.',
+    para2: 'Playing a mixture of original and cover music Jacob can be found playing venues around Ireland along with paying his dues by busking.'
   },
   {
     header: 'https://ytimg.googleusercontent.com/vi/1LvxwPxfV8M/mqdefault.jpg',
