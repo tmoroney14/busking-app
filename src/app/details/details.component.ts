528993ff04f429398cd9086ced8a078dbec5f6dd
tmoroney14 <thomas.moroney1010@gmail.com>
2019-09-01 00:32:59 +0100

    5 files modified

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -4,6 +4,10 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatExpansionModule } from '@angular/material/expansion';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 import { SafePipe } from '../safe.pipe';
 import { buskers } from '../buskers';
@@ -17,7 +21,11 @@ import { buskers } from '../buskers';
 
 export class DetailsComponent implements OnInit {
   panelOpenState = false;
-
+  
+  faTwitter = faTwitter;
+  faInstagram = faInstagram;
+  faFacebook = faFacebook;
+  faYoutube = faYoutube;
   
     
 
