3f0bf6380c330ac9f22aec39d0c4aa2fdfb3b671
tmoroney14 <thomas.moroney1010@gmail.com>
2019-09-15 14:32:03 +0100

    5 files modified, 5 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import $ from "jquery";
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -24,7 +23,6 @@ import { BuskersListComponent } from './buskers-list/buskers-list.component';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
 import { SafePipe } from './safe.pipe';
-import { ParallaxComponent } from './parallax/parallax.component';
 
 
 
@@ -59,7 +57,6 @@ import { ParallaxComponent } from './parallax/parallax.component';
     HomeComponent,
     DetailsComponent,
     SafePipe,
-    ParallaxComponent,  
   ],
 
   bootstrap: [ AppComponent ]
--- src/app/script.store.ts
+++
@@ -1,8 +0,0 @@
-interface Scripts {
-    name: string;
-    src: string;
-}  
-export const ScriptStore: Scripts[] = [
-    {name: 'filepicker', src: 'https://api.filestackapi.com/filestack.js'},
-    {name: 'rangeSlider', src: '../../../assets/js/ion.rangeSlider.min.js'}
-];
